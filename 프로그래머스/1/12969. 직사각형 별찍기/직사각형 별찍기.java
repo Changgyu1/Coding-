c16707cc9f9d0ab0d4496e6a0af8331419c4c2cb
ChangGyu <100547448+Changgyu1@users.noreply.github.com>
2024-06-02 23:02:20 +0900

    Update 직사각형 별찍기.java

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        String star = ""; // 별 개수
-        for(int i = 1; i <= b; i++){ // 열
-            for(int j = 1; j <= a; j++){ // 행( * 개수)
+        int a = sc.nextInt(); // 열
+        int b = sc.nextInt(); // 행
+        
+        for(int i = 1; i <= b; i++){ // 행
+            String star = ""; // 행 마다 초기화
+            for(int j = 1; j <= a; j++){ // 열( * 개수)
                 star += "*";
             }
-            System.out.println(star); // 행 출력
-            star =""; // 행 초기화
+            System.out.println(star); // 각 행 출력
         }
     }
 }
